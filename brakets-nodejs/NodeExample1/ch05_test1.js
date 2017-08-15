a488bfe7f66a2a978710b54338a45320ee7a52c2
haidn94 <haidn94@tachyontech.co.kr>
2017-08-16 01:28:39 +0900

    second commit

---
+++ brakets-nodejs/NodeExample1/ch05_test1.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+// 웹 서버 객체를 만든다.
+var server = http.createServer();
+
+// 웹 서버를 시작하여 3000번 포트에서 대기한다.
+var port = 3000;
+server.listen(port, function() {
+    console.log('웹 서버가 시작되었습니다. : %d', port);
+});
